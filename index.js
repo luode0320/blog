50477802555ab1f36066e916c135bfdb9df8f00e
欧皇小德子 <74521604+Rod0320@users.noreply.github.com>
2023-10-04 17:03:02 +0800

    调整为直接引用github的文件

---
+++ index.js
@@ -0,0 +1,11 @@
+const path = require('path');
+const fs = require('fs');
+const express = require('express');
+const app = express();
+
+app.use(express.static(path.join(__dirname, '/')));
+
+const port = 4000;
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
